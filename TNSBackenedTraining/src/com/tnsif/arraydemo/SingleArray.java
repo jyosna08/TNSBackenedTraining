021cc5f1f9d16b980f3c0b2c9593b4ff306f1337
jyosna <jyosna@DESKTOP-MRJ9SD0>
2025-08-19 19:55:54 +0530

    Array1D

--- TNSBackenedTraining/src/com/tnsif/arraydemo/SingleArray.java
+++ TNSBackenedTraining/src/com/tnsif/arraydemo/SingleArray.java
@@ -1,11 +1,30 @@
 package com.tnsif.arraydemo;
-
+// Demo for single dimensional array
 public class SingleArray {
 
 	public static void main(String[] args) {
+		
+		// declaration 
+		//int[] arr;
+		
+		//int []arr;
+		
+		//declaration + initialization
 		int []arr = {1,2,3};
-		System.out.println("the element in the array are "+ arr);
+		int[] number = new int[5]; 
+		number[0]=10;
+		number[1]=11;
+		number[2]=12;
+		number[3]=13;
+		number[4]=14;
+		
+		for(int i=0;i<number.length;i++) {
+			System.out.println("the element in the array are "+ number[i]);  // using for loop
+			}
+		for(int num:arr){
+			System.out.println("the element in the array are "+ num);    // enhances or for each for loop
 
+		}
 	}
 
 }
