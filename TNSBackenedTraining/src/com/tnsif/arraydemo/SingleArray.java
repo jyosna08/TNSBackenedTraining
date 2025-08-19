25e780018397f0f2f69c6a3077eff2d6e48333e6
jyosna <jyosna@DESKTOP-MRJ9SD0>
2025-08-19 14:42:47 +0530

    Array1D

---
+++ TNSBackenedTraining/src/com/tnsif/arraydemo/SingleArray.java
@@ -0,0 +1,11 @@
+package com.tnsif.arraydemo;
+
+public class SingleArray {
+
+	public static void main(String[] args) {
+		int []arr = {1,2,3};
+		System.out.println("the element in the array are "+ arr);
+
+	}
+
+}
