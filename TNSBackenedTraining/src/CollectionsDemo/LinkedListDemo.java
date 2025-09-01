14baf031e73d4e574450261703020ede7f2a6c33
jyosna <jyosna@DESKTOP-MRJ9SD0>
2025-09-01 19:42:37 +0530

    LinkedlistDemo

---
+++ TNSBackenedTraining/src/CollectionsDemo/LinkedListDemo.java
@@ -0,0 +1,36 @@
+package CollectionsDemo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+//		LinkedList a1=new LinkedList();
+//		a1.add(10);
+//		a1.add("Hello");
+//		a1.add("Hello");
+//		a1.add(23.09f);
+//		a1.add("");
+//		a1.addFirst(90);
+//		a1.addLast(100);
+//		a1.remove(1);
+//		a1.add(2,13);
+//		
+//		System.out.println("The data in the ArrayList");
+//		System.out.println(a1);
+		
+		
+		LinkedList<Integer> a2 = new LinkedList<Integer>();
+		a2.add(10);
+		a2.add(23);
+		a2.add(67);
+	
+		
+		System.out.println("The data in the ArrayList");
+		System.out.println(a2);
+		
+
+	}
+
+}
